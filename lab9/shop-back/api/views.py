d2b49fde40b22fcb263c952d8ad26e07c619a824
Давид <david.sebelev@gmail.com>
2026-04-02 10:30:05 +0500

    practice

--- lab9/shop-back/api/views.py
+++ lab9/shop-back/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import Category, Product
 from .serializers import CategorySerializer, ProductSerializer
+from rest_framework import filters
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
@@ -17,3 +18,7 @@ class CategoryViewSet(viewsets.ModelViewSet):
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['name', 'description']
+    ordering_fields = ['price', 'name']
+    ordering = ['id']
